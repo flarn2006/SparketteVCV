af2e074455316e9010f44a8ad9ff229471f53619
Sara Sparks <flarn2006@gmail.com>
2022-09-14 20:33:06 -0400

    Add LEDMatrix

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -14,6 +14,7 @@ void init(Plugin* p) {
 	p->addModel(modelNoteClassifier);
 	p->addModel(modelPolyRepeater);
 	p->addModel(modelFastClock);
+	p->addModel(modelLEDMatrix);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -14,3 +14,4 @@ extern Model* modelEllie;
 extern Model* modelNoteClassifier;
 extern Model* modelPolyRepeater;
 extern Model* modelFastClock;
+extern Model* modelLEDMatrix;

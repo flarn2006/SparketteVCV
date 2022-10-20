b2d808afb9206f9e524fe66b03586d1ee6daf3cb
Sara Sparks <flarn2006@gmail.com>
2022-10-20 19:48:32 -0400

    Forgot to commit these changes

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -2,8 +2,8 @@
 
 namespace sparkette {
 
-	float applyScaleOffset(rack::engine::Input& input, rack::engine::Param& scale, rack::engine::Param& offset) {
-		float cv = input.getVoltage() / 10;
+	float applyScaleOffset(float voltage, rack::engine::Param& scale, rack::engine::Param& offset) {
+		float cv = voltage / 10;
 		float sc = scale.getValue();
 		float of = offset.getValue();
 		return of + sc * cv;

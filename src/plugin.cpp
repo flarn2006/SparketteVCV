f1c1289a608b3c0cafa23b199c8ef188777d79e8
Sara Sparks <flarn2006@gmail.com>
2022-10-20 18:52:50 -0400

    Add Color Wheel (HSV2RGB) module

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -13,6 +13,7 @@ void init(Plugin* p) {
 	p->addModel(modelNoteClassifier);
 	p->addModel(modelPolyRepeater);
 	p->addModel(modelRGBMatrix);
+	p->addModel(modelHSV2RGB);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.

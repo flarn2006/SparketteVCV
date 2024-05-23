f1847f2ebdd65fcc41fdf09c4861dd6871f81e46
Sara Sparks <flarn2006@gmail.com>
2024-05-23 19:37:04 -0400

    Remove unused legacy 7-segment display class/font

--- src/Widgets.hpp
+++ src/Widgets.hpp
@@ -12,31 +12,6 @@ struct GlowingWidget : TWidget {
 	}
 };
 
-class SevenSegmentDisplay : public TransparentWidget {
-
-	bool needsUpdate = false;
-	int value, digits;
-	std::string text, backtext;
-	bool hexMode = false;
-	std::shared_ptr<Font> font;
-
-	void drawPart(const DrawArgs& args, bool isBack);
-
-public:
-	NVGcolor color;
-	NVGcolor offColor;
-
-	SevenSegmentDisplay();
-	void draw(const DrawArgs& args) override;
-	void drawLayer(const DrawArgs& args, int layer) override;
-	void step() override;
-	int getValue() const;
-	void setValue(int newValue, int digits);
-	void setHexMode(bool state);
-	void setColor(const NVGcolor& newColor);
-
-};
-
 struct CKSSWithLine : app::SvgSwitch {
 	CKSSWithLine();
 };
